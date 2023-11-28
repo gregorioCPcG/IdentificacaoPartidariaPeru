717400cbef35476b1a5a2d0f7c2a42231b726fa3
gregorioCPcG <93200696+gregorioCPcG@users.noreply.github.com>
2023-11-28 11:02:32 -0300

    Update recodificação original.R

--- recodificação original.R
+++ recodificação original.R
@@ -36,21 +36,21 @@ table(df$S003, df$S002VS)
 df -> df2
 
 df <- remove_labels(df)
-# Lista de variáveis desejadas
+# Lista de variables deseadas
 variaveis_desejadas <- c("X001", "X025R", "X048ISO", "X049", "X051", "X007", "X003R2", "X028", "F025", "E023", "E033",
                          "E179WVS", "E069_06", "E069_07", "E069_12", "E069_17", "F028", "F118", "F120", "E036",
                          "E037", "E040", "E114", "E116", "E117", "E235","X045","X047R_WVS","E039")
 #29
-# Verificar se as variáveis estão presentes no dataframe
+# Comprobar si hay variables presentes en el marco de datos.
 variaveis_presentes <- colnames(df) %in% variaveis_desejadas
 
-# Exibir as variáveis que estão presentes
+# Mostrar las variables que están presentes.
 print(colnames(df)[variaveis_presentes])
 n <- colnames(df)[variaveis_presentes]
-# Verificar as variáveis ausentes em relação à lista desejada
+# Verifique las variables que faltan con la lista deseada
 variaveis_faltando <- variaveis_desejadas[!variaveis_desejadas %in% n]
 
-# Exibir as variáveis faltando
+# Mostrar variables faltantes
 print(variaveis_faltando)#zero!
 
 
@@ -65,5 +65,5 @@ df2 <- subset(df2, select = c(S002VS, S003, X001, X025R, X048ISO, X049, X051, X0
                               E033, E179WVS, E069_06, E069_07, E069_12, E069_17, F028, F118, F120, E036,
                               E037, E040, E114, E116, E117, E235, X045, X047R_WVS,E039))
 df2 <- write_sav(df2,"dfao.sav")
-#consultar tudo referente à códigos nos arquivos F00012255-WVS_TimeSeries_1981_2020_CountrySpecificCodes.xlsx e 
+#consultar todo lo referente a códigos en los archivos F00012255-WVS_TimeSeries_1981_2020_CountrySpecificCodes.xlsx e 
 #F00003844-WVS_Time_Series_List_of_Variables_and_equivalences_1981_2022_v3_1.xlsx
